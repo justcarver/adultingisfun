2d69effa813425494df9f6ba5184beb5db7ad172
Justin Carver <carver@justcarver.com>
2016-02-20 22:51:58 -0500

    Added "active" column to Task model
    Adding active date to tasks, to better track when tasks are active.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160205065543) do
+ActiveRecord::Schema.define(version: 20160221034714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20160205065543) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "category"
+    t.date     "active"
   end
 
   create_table "users", force: :cascade do |t|
